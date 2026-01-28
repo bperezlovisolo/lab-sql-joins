f8adec1fb6a7bd99238ce5a1311533446c2bbe45
bperezlovisolo <b.perezlovisolo@gmail.com>
2026-01-28 17:12:02 +0100

    lab-sql-join

---
+++ lab-sql-joins.sql
@@ -0,0 +1,100 @@
+USE sakila;
+
+SELECT
+category_id,
+COUNT(film_id) as num_films
+FROM film_category
+GROUP BY category_id;
+
+
+-- store_id y address_id en store
+-- address_id y city_id en address
+-- city_id, city y country_ id en city
+-- country_id y country en country
+
+SELECT 
+st.store_id,
+c.city,
+co.country
+FROM country AS co
+INNER JOIN city AS c
+ON c.country_id = co.country_id
+INNER JOIN address AS ad
+ON c.city_id = ad.city_id
+INNER JOIN store AS st
+ON ad.address_id = st.address_id;
+
+SELECT *
+FROM store;
+
+SELECT
+st.store_id,
+SUM(pay.amount)
+FROM store AS st
+LEFT JOIN payment AS pay
+ON st.manager_staff_id = pay.staff_id
+GROUP BY st.store_id;
+
+SELECT *
+FROM film_category;
+
+SELECT
+c.name,
+AVG(length) AS avg_length
+FROM film AS f
+INNER JOIN film_category AS fc
+ON f.film_id = fc.film_id
+INNER JOIN category AS c
+ON c.category_id = fc.category_id
+GROUP BY c.name
+ORDER BY avg_length DESC;
+
+SELECT
+c.name,
+AVG(length) AS avg_length
+FROM film AS f
+INNER JOIN film_category AS fc
+ON f.film_id = fc.film_id
+INNER JOIN category AS c
+ON c.category_id = fc.category_id
+GROUP BY c.name
+ORDER BY avg_length DESC
+LIMIT 1;
+
+SELECT *
+FROM inventory;
+
+SELECT 
+title,
+COUNT(rental_id) AS num_rental
+FROM rental AS r
+INNER JOIN inventory AS i
+ON r.inventory_id = i.inventory_id
+INNER JOIN film as f
+ON f.film_id = i.film_id
+GROUP BY title
+ORDER BY num_rental DESC
+LIMIT 10;
+
+SELECT
+  CASE
+    WHEN COUNT(*) > 0 THEN 'Available'
+    ELSE 'Not Available'
+  END AS availability
+FROM inventory
+WHERE film_id = 1 AND store_id = 1;
+
+SELECT *
+FROM inventory;
+
+SELECT 
+title,
+  CASE
+    WHEN i.inventory_id IS NOT NULL THEN 'Available'
+    ELSE 'NOT available'
+  END AS availability
+FROM film AS f
+LEFT JOIN inventory AS i
+  ON f.film_id = i.film_id;
+
+
